8517c6e13f5dc8c7d95ac4585d902c5c6506f7eb
Parvesh Nigam <mr.nigam.r01@gmail.com>
2026-04-07 22:38:38 +0530

    Build User and Video models

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,38 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) =>{
+    try{
+        if(!localFilePath) return null;
+
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        });
+        
+         fs.unlink(localFilePath, (err) => {
+            if (err) console.error("unlink failed", err);
+        });
+
+        // console.log(`file is uploaded on cloudinary ${respons    e.url}`);
+        return response;
+    }catch(err){
+        // remove temp file if upload fails
+        //fs.unlinkSync(localFilePath);
+
+        fs.unlink(localFilePath, (err) => {
+            if (err) console.error("unlink failed", err);
+        });
+        
+        return null;
+    }
+};
+
+export {uploadOnCloudinary};
+
+
