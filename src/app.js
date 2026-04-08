18d0fe5dc5b473d468e5eec3dbfd9a37dbe62a5d
Parvesh Nigam <mr.nigam.r01@gmail.com>
2026-04-09 04:49:10 +0530

    debugging for form-data

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import multer from "multer";
+
 
 const app = express();
+const upload = multer();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -17,6 +20,8 @@ app.use(express.urlencoded({
     extended: true,
     limit: "16kb"
 }));
+
+app.use(upload.none()); // for text-only form-data
 
 app.use(express.static("public"))
 
